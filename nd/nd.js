2a787e3b7919c99a63dd4ce4819b26b48a67399d
JuliusBTI <j.baksinskas1988@gmail.com>
2024-07-18 10:20:22 +0300

    first commit

---
+++ nd/nd.js
@@ -0,0 +1,98 @@
+const pokemonai = [
+    {
+      img:"https://img.pokemondb.net/artwork/avif/charizard.avif" ,
+      name:'Charizard',
+      Species:'Flame',
+      Height: 1.7,
+      Weight:90.5,
+      readmore:"https://pokemondb.net/pokedex/charizard",
+    },
+    {
+        img:"https://img.pokemondb.net/artwork/avif/bulbasaur.avif",
+        name:'Bulbasaur',
+        Species:'Seed',
+        Height:0.7,
+        Weight:6.9,
+        readmore:"https://pokemondb.net/pokedex/bulbasaur",
+      },
+      {
+        img:"https://img.pokemondb.net/artwork/avif/meganium.avif",
+        name:'Meganium',
+        Species:'Herb',
+        Height:1.8,
+        Weight:100.5,
+        readmore:"https://pokemondb.net/pokedex/meganium",
+      },
+      {
+        img:"https://img.pokemondb.net/artwork/avif/pikachu.avif",
+        name:'Pikachu',
+        Species:'Mouse',
+        Height:0.4,
+        Weight:6.0,
+        readmore:"https://pokemondb.net/pokedex/pikachu",
+      },
+      {
+        img:"https://img.pokemondb.net/artwork/avif/froslass.avif",
+        name:'Froslass',
+        Species:'Snow',
+        Height:1.3,
+        Weight:26.6,
+        readmore:"https://pokemondb.net/pokedex/froslass",
+      },
+      {
+        img:"https://img.pokemondb.net/artwork/avif/meganium.avif",
+        name:'Dialga',
+        Species:'Temporal',
+        Height:5.4, 
+        Weight:683.0,
+        readmore:"https://pokemondb.net/pokedex/dialga",
+      },
+      {
+        img:"https://img.pokemondb.net/artwork/avif/palkia.avif",
+        name:'Palkia',
+        Species:'Spatial',
+        Height:4.2,
+        Weight:336.0,
+        readmore:"https://pokemondb.net/pokedex/palkia",
+      },
+      {
+        img:"https://img.pokemondb.net/artwork/avif/cubone.avif",
+        name:'Cubone',
+        Species:'Lonely',
+        Height:0.4,
+        Weight:6.5,
+        readmore:"https://pokemondb.net/pokedex/cubone",
+      },
+      {
+        img:"https://img.pokemondb.net/artwork/avif/lickitung.avif",
+        name:'Lickitung',
+        Species:'Licking',
+        Height:1.2,
+        Weight:65.5,
+        readmore:"https://pokemondb.net/pokedex/lickitung",
+      },
+      {
+        img:"https://img.pokemondb.net/artwork/avif/kangaskhan.avif",
+        name:'Kangaskhan',
+        Species:'Parent',
+        Height:2.2,
+        Weight:80.0,
+        readmore:"https://pokemondb.net/pokedex/kangaskhan",
+      },
+];              
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
